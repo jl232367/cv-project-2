9962b80a46d2d2e60b05007905fa8424cef79f36
Patrick <patches72790@gmail.com>
2021-09-11 13:52:28 -0500

    updated conditional rendering

--- src/components/generalinfo.js
+++ src/components/generalinfo.js
@@ -1,54 +1,62 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-
-class GeneralInfo extends Component { 
+class GeneralInfo extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            firstName: '',
-            lastName: '',
-            email: '',
-            phoneNumber: '', 
-
+            formSubmitted: false,
+            firstName: "",
+            lastName: "",
+            email: "",
+            phoneNumber: "",
+            firstNameInput: "",
+            lastNameInput: "",
+            emailInput: "",
+            phoneNumberInput: "",
         };
         this.saveFirstName = this.saveFirstName.bind(this);
         this.saveLastName = this.saveLastName.bind(this);
         this.saveEmail = this.saveEmail.bind(this);
         this.savePhoneNumber = this.savePhoneNumber.bind(this);
-        this.updateInfo =this.updateInfo.bind(this);
+        this.updateInfo = this.updateInfo.bind(this);
+        this.renderCurrentInfo = this.renderCurrentInfo.bind(this);
     }
 
     saveFirstName(event) {
-        this.setState({firstNameInput: event.target.value});
+        this.setState({ firstNameInput: event.target.value });
     }
 
     saveLastName(event) {
-        this.setState({lastNameInput: event.target.value});
+        this.setState({ lastNameInput: event.target.value });
     }
 
     saveEmail(event) {
-        this.setState({emailInput: event.target.value});
+        this.setState({ emailInput: event.target.value });
     }
 
     savePhoneNumber(event) {
-        this.setState({phoneNumberInput: event.target.value});
+        this.setState({ phoneNumberInput: event.target.value });
     }
     // This should update the info from the different feilds then render the updated content
     updateInfo(event) {
-        //These shoudl all have updated states already updated on any change to specific form feild. 
-        let {firstNameInput, lastNameInput, emailInput, phoneNumberInput} = this.state;
+        //These shoudl all have updated states already updated on any change to specific form feild.
+        let { firstNameInput, lastNameInput, emailInput, phoneNumberInput } =
+            this.state;
         // series of states updated
-        this.setState({firstName: firstNameInput});
-        this.setState({lastName: lastNameInput});
-        this.setState({email: emailInput});
-        this.setState({phoneNumber: phoneNumberInput});
+        this.setState({ firstName: firstNameInput });
+        this.setState({ lastName: lastNameInput });
+        this.setState({ email: emailInput });
+        this.setState({ phoneNumber: phoneNumberInput });
         // Trying to add a function to render the information now stored in the compoenent
-        
 
-        //THIS IS WHAT i AM TRYING TO GET TO RENDER ON SUBMIT
-        const currentInfo = (
+        this.setState({ formSubmitted: true });
+        event.preventDefault();
+    }
+
+    renderCurrentInfo() {
+        return (
             <div>
                 <h2>{this.state.firstName}</h2>
                 <h2>{this.state.lastName}</h2>
@@ -56,38 +64,53 @@ class GeneralInfo extends Component {
                 <h2>{this.state.phoneNumber}</h2>
             </div>
         );
-        alert();
-        //ReactDOM.render(currentInfo, document.getElementById('generalInfoContainer'))
-            
-        event.preventDefault();
     }
 
     //THIS IS THE INITIAL RENDER THAT NEEDS TO BE SWAPPED WITH THE UPDATED INFO ON SUBMISSION
     render() {
-        return (
-            <div id='generalInfoContainer'>
+        const { formSubmitted } = this.state;
+
+        return (!formSubmitted ? (
+            <div id="generalInfoContainer">
                 <form onSubmit={this.updateInfo}>
                     <label>
                         First Name:
-                        <input type="text" value= {this.state.firstNameInput } onChange={this.saveFirstName}/>
+                        <input
+                            type="text"
+                            value={this.state.firstNameInput}
+                            onChange={this.saveFirstName}
+                        />
                     </label>
                     <label>
-                        Last Name: 
-                        <input type="text" value={this.state.lastNameInput} onChange={this.saveLastName}/>
+                        Last Name:
+                        <input
+                            type="text"
+                            value={this.state.lastNameInput}
+                            onChange={this.saveLastName}
+                        />
                     </label>
                     <label>
                         Email:
-                        <input type="email" value = {this.state.emailInput} onChange={this.saveEmail}/>
+                        <input
+                            type="email"
+                            value={this.state.emailInput}
+                            onChange={this.saveEmail}
+                        />
                     </label>
                     <label>
                         Phone Number:
-                        <input type="tel" value = {this.state.phoneNumberInput}onChange={this.savePhoneNumber}/>
+                        <input
+                            type="tel"
+                            value={this.state.phoneNumberInput}
+                            onChange={this.savePhoneNumber}
+                        />
                     </label>
                     <input type="submit" value="submit" />
-                
                 </form>
             </div>
-        )
+        ) : (
+            this.renderCurrentInfo()
+        ));
     }
 }
-export default GeneralInfo; 
+export default GeneralInfo;
